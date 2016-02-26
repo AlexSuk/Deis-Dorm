3834a06cf7b84c34b1887f609d4809f415968738
zcm0705 <cuimei@brandeis.edu>
2016-02-26 17:18:21 -0500

    Add association

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,2 +1,4 @@
 class Line < ActiveRecord::Base
+  has_many :posts
+  belongs_to :board 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ActiveRecord::Base
+  has_many :pictures
+  belongs_to :user
+  belongs_to :building
 end
