074d0ca39fcffa77b138b44f5dc797e9622b1a1d
jmaeda <thecopyholder@gmail.com>
2016-04-01 15:35:24 -0400

    fix seed data for quads

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,17 @@
 
 #require 'faker'
 
-Quad.create(name: "Ziv", rating: 4.1, info: "Quad with four buildings. For Juniors and Seniors. Lower campus")
-Quad.create(name: "East", rating: 3.7, info: "Two buildings connected with a hallway. For Sophomores. Upper campus")
-Quad.create(name: "Massell", rating: 5.0, info: "Freshman dorms in lower campus. Four buildings")
-Quad.create(name: "Ridgewood", rating: 3.2, info: "The housing that all juniors want.")
-Quad.create(name: "North", rating: 2.0, info: "Cable feels like a prison, but it will get better soon")
-Quad.create(name: "Usen Castle", rating: 3.5, info: "Only called castle to attract students, no one actually wants to live in it")
-Quad.create(name: "567 South Street", rating: 3.0, info: "Only outcasts want to live here")
-Quad.create(name: "Charles River Apartments", rating: 1.0, info: "spiders everywhere")
-Quad.create(name: "Foster Mods Housing", rating: 4.5, info: "Expensive but worth")
-Quad.create(name: "Rosenthal", rating: 4.0, info: "Worse version of Ziv")
-Quad.create(name: "Village", rating:4.0, info: "Housing for midyears, study abroad students, and sophomores")
+Quad.create(name: "Ziv", rating: 4.1, info: "Quad with four buildings. For Juniors and Seniors. Lower campus", years: "0011")
+Quad.create(name: "East", rating: 3.7, info: "Two buildings connected with a hallway. For Sophomores. Upper campus", years: "0100")
+Quad.create(name: "Massell", rating: 5.0, info: "Freshman dorms in lower campus. Four buildings", years: "1000")
+Quad.create(name: "Ridgewood", rating: 3.2, info: "The housing that all juniors want.", years: "0011")
+Quad.create(name: "North", rating: 2.0, info: "Cable feels like a prison, but it will get better soon", years: "1000")
+Quad.create(name: "Usen Castle", rating: 3.5, info: "Only called castle to attract students, no one actually wants to live in it", years: "0100")
+Quad.create(name: "567 South Street", rating: 3.0, info: "Only outcasts want to live here", years: "0100")
+Quad.create(name: "Charles River Apartments", rating: 1.0, info: "spiders everywhere", years: "0011")
+Quad.create(name: "Foster Mods Housing", rating: 4.5, info: "Expensive but worth", years: "0001")
+Quad.create(name: "Rosenthal", rating: 4.0, info: "Worse version of Ziv", years: "0100")
+Quad.create(name: "Village", rating:4.0, info: "Housing for midyears, study abroad students, and sophomores", years: "1110")
 
 Board.create(name: "Ziv Quad", description: "Better version of Rosie")
 Board.create(name: "Ridgewood", description: "nice housing")
