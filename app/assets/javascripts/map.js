6f691c13a1b883cf1b64b9fbf0326ca3679f6b09
zcm0705 <cuimei@brandeis.edu>
2016-04-11 16:07:50 -0400

    map - add checkbox but broken

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,13 +1,32 @@
+// function map(){
+//   console.log("here");
+//   if(!Gmaps){
+//     console.log("no gmaps");
+//     setTimeout(function(){console.log(map)},1000);
+//     return;
+//   }else{
+//     console.log("we got gmaps");
+//   }
 
 var json_array = [
   { lat: 42.364018, lng: -71.260718, name: 'Ridgewood', infowindow: "Here is Ridgewood Quad." },
   { lat: 42.369610, lng: -71.255711, name: 'North', infowindow: "Here is North Quad." },
   { lat: 42.367565, lng: -71.255646, name: 'Castle', infowindow: "Here is a beautiful Castle." }
 ];
+var places_of_interest = [
+  {lat: 42.366327, lng: -71.258744, name: "Dunkin' Donuts", infowindow: "food info and shop info"},
+  {lat: 42.368043, lng: -71.256617, name: "Usdan Dinning Hall", infowindow: "Lower Usdan Dinning hall"},
+  {lat: 42.367854, lng: -71.258671, name: "Starbucks", infowindow: "Starbucks in Farber Library"},
+  {lat: 42.365730, lng: -71.260188, name: "Einstein Bros Bagels",infowindow: "Selling bagels and softdrinks here"},
+  {lat: 42.366412, lng: -71.260546, name: "Sherman Dinning Hall", infowindow: "Student dinning hall"}
+];
+// console.log("second");
+// var json_array = building_markers
 var initialized = false;
 var Gmaps;
 var handler = Gmaps.build('Google');
 var markers = [];
+var interest_markers = [];
 var map = handler.buildMap({ internal: {id: 'map'} }, function(){
   markers = handler.addMarkers([
     {
@@ -25,10 +44,11 @@ var map = handler.buildMap({ internal: {id: 'map'} }, function(){
   handler.fitMapToBounds();
   handler.getMap().setZoom(16); //control zoom level (0-21)
   if(!initialized) createSidebar(json_array);
+
 });
 
 if(navigator.geolocation)
-   navigator.geolocation.getCurrentPosition(displayOnMap);
+    navigator.geolocation.getCurrentPosition(displayOnMap);
 
 function createSidebar(json_array){
   initialized = true;
@@ -42,6 +62,7 @@ function createSidebar(json_array){
 };
 
 
+
 function find(marker){
   // move to marker
   marker.setMap(handler.getMap()); //because clusterer removes map property from marker
@@ -52,6 +73,7 @@ function find(marker){
 
 function create(obj){
   // create marker
+  destroyAll();
   var marker=handler.addMarker(obj);
   marker.setMap(map);
   find(marker);
@@ -82,3 +104,54 @@ $("#search").on("input",function(ev){
 });
 
 $("body > #sidebar_container").remove();// clean up after Google
+// }
+
+// create an array to store all markers of places_of_interest
+function create_interest_marker(places) {
+  for (var i = 0; i< places.length; i ++){
+    var marker = handler.addMarker(places[i]);
+    // interest_markers.push(marker);
+  }
+}
+
+// == shows all markers of places_of_interest and ensures the checkbox is checked ==
+function show(places_of_interest) {
+  interest_markers = create_interest_marker(places_of_interest);
+
+  // var regEx = new RegExp("[" + places_of_interest + "]")
+  // for (var i=0; i < interest_markers.length; i++) {
+  //   if (Gmaps.map.markers[i].cat) {
+  //     if (Gmaps.map.markers[i].cat.match(regEx)) {
+  //       Gmaps.map.showMarker(Gmaps.map.markers[i]);
+  //     }
+  //   }
+  // }
+  // == check the checkbox ==
+  // document.getElementById(places_of_interest+"box").checked = true;
+}
+
+// == hides all markers of  places_of_interest, and ensures the checkbox is cleared ==
+function hide(places_of_interest) {
+  destroyAll();
+  // var regEx = new RegExp("[" + places_of_interest + "]")
+  // for (var i=0; i < Gmaps.map.markers.length; i++) {
+  //   if (Gmaps.map.markers[i].cat) {
+  //     if (Gmaps.map.markers[i].cat.match(regEx)) {
+  //       Gmaps.map.hideMarker(Gmaps.map.markers[i]);
+  //     }
+  //   }
+  // }
+  // == clear the checkbox ==
+  // document.getElementById(places_of_interest+"box").checked = false;
+  // == close the info window, in case its open on a marker that we just hid
+  // Gmaps.map.infowindow.close();
+}
+
+// == a checkbox has been clicked ==
+function boxclick(box) {
+  if (box.checked) {
+    show(places_of_interest);
+  } else {
+    hide(places_of_interest);
+  }
+}
