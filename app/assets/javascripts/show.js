2bf20d88149874e38ebdaa4427ce05855405f125
zcm0705 <cuimei@brandeis.edu>
2016-05-01 12:03:04 -0400

    populate sidebar and change data layer to be colorful

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -15,10 +15,10 @@ $(document).ready(function(){
   $('input[type=range]').on('input change',function(){
     var input = $(this);
     updateRangeValue(input);
-  });    
+  });
 });
 window.onresize=function(){
-  $('input[type=range]').each(function(index,value){updateRangeValue(value)});  
+  $('input[type=range]').each(function(index,value){updateRangeValue(value)});
 }
 
 
@@ -34,13 +34,13 @@ function updateRangeValue(input){
   var selectedColor = "#428bca";
   var nonSelectedColor = "#ddd"
   var value = input.val();
-  var maximum = input.attr('max'); 
-  var rangeLength = input.attr('max') - input.attr('min') + 1; 
+  var maximum = input.attr('max');
+  var rangeLength = input.attr('max') - input.attr('min') + 1;
   var inputWidth = input.width(); //323 width
   var background = getRangeGradient(selectedColor, nonSelectedColor, value, maximum);
   var numLocation=(value/10)*(inputWidth-32);
   input.next('.text').css({'left': numLocation + 'px'});
   input.next('.text').css({'top': -10 + 'px'}); //circle radius + runnable track height
   input.next('.text').html(value);
-  input.css('background', background); 
+  input.css('background', background);
 }
