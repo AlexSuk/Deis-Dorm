95d37410c57c90f9b4a2be23b0368630bbf0b8c3
ctakahashi <tachi6@brandeis.edu>
2016-05-02 16:05:28 -0400

    Attempt to add ratings to rooms from seed reviews

--- app/jobs/room_counter_job.rb
+++ app/jobs/room_counter_job.rb
@@ -1,31 +1,48 @@
-class TagCounterJob
+class RoomCounterJob
   include SuckerPunch::Job
   workers 4
 
   def perform(args)
-    room_id = args[:room_id]
-    c_rating =
+    room = args[:room]
+    c_rating = args[:cleanliness]
     n_rating = args[:noisiness]
     l_rating = args[:light]
     s_rating = args[:socialness]
-    $redis.pipelined do
-      unless c_rating.nil?
-        $redis.hincrby("r_#{room_id}", "c_n", "1")
-        $redis.hincrby("r_#{room_id}", "c_sum", c_rating.to_s)
-      end
-      unless n_rating.nil?
-        $redis.hincrby("r_#{room_id}", "n_n", "1")
-        $redis.hincrby("r_#{room_id}", "n_sum", n_rating.to_s)
-      end
-      unless l_rating.nil?
-        $redis.hincrby("r_#{room_id}", "l_n", "1")
-        $redis.hincrby("r_#{room_id}", "l_sum", l_rating.to_s)
-      end
-      unless s_rating.nil?
-        $redis.hincrby("r_#{room_id}", "s_n", "1")
-        $redis.hincrby("r_#{room_id}", "s_sum", s_rating.to_s)
-      end
-    end
+    room_stats = $redis.hgetall("r_#{room.id}")
 
+    c_avg = calc_avg("c", c_rating, room_stats)
+    n_avg = calc_avg("n", n_rating, room_stats)
+    l_avg = calc_avg("l", l_rating, room_stats)
+    s_avg = calc_avg("s", s_rating, room_stats)
+
+    room.update(cleanliness: c_avg, noisiness: n_avg, light: l_avg, socialness: s_avg)
+
+    $redis.pipelined do
+      incrby("c", c_rating, room.id)
+      incrby("n", n_rating, room.id)
+      incrby("l", l_rating, room.id)
+      incrby("s", s_rating, room.id)
+      # unless n_rating.nil?
+      #   $redis.hincrby("r_#{room.id}", "n_n", "1")
+      #   $redis.hincrby("r_#{room.id}", "n_sum", n_rating.to_s)
+      # end
+    end
+  end
+
+  def calc_avg(attr, rating, roomd_stats)
+    avg = 0.0
+    if rating == 0
+      avg = room_stats["#{attr}_sum"].to_f / room_stats["#{attr}_n"].to_f
+    else
+      avg = (room_stats["#{attr}_sum"].to_f + rating.to_f) / (room_stats["#{attr}_n"].to_f + 1)
+    end
+    return avg
+  end
+
+  def incrby(attr, rating, room_id)
+    unless rating.nil?
+      $redis.hincrby("r_#{room_id}", "#{attr}_n", "1")
+      $redis.hincrby("r_#{room_id}", "#{attr}_sum", rating.to_s)
+    end
   end
 end
