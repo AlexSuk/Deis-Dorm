0d3f61eca9039be3cad4222b813c10d51da8db8d
zcm0705 <cuimei@brandeis.edu>
2016-05-02 16:18:55 -0400

    try to commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,62 +36,52 @@ Board.create(name: "FAQs/Questions and Answers", description: "etc")
 
 
 #ziv buildings
-ziv = Quad.find_by(name: "Ziv")
-Building.create(name: "Ziv 127", longitude: -71.2616387, latitude: 42.3633962, description: "Rightmost Ziv", is_kind: "dorm", quad_id: ziv.id, years: ziv.years)
-Building.create(name: "Ziv 128", longitude: -71.2615093, latitude: 42.3630568, description: "Ziv to the left of 127", is_kind: "dorm", quad_id: ziv.id, years: ziv.years)
-Building.create(name: "Ziv 129", longitude: -71.2610225, latitude: 42.3629225, description: "Ziv to the right of 130", is_kind: "dorm", quad_id: ziv.id, years: ziv.years)
-Building.create(name: "Ziv 130", longitude: -71.2608863, latitude: 42.3632783, description: "Leftmost Ziv", is_kind: "dorm", quad_id: ziv.id, years: ziv.years)
+Building.create(name: "Ziv 127", longitude: -71.2616387, latitude: 42.3633962, description: "Rightmost Ziv", is_kind: "dorm", quad_id: Quad.find_by(name: "Ziv").id)
+Building.create(name: "Ziv 128", longitude: -71.2615093, latitude: 42.3630568, description: "Ziv to the left of 127", is_kind: "dorm", quad_id: Quad.find_by(name: "Ziv").id)
+Building.create(name: "Ziv 129", longitude: -71.2610225, latitude: 42.3629225, description: "Ziv to the right of 130", is_kind: "dorm", quad_id: Quad.find_by(name: "Ziv").id)
+Building.create(name: "Ziv 130", longitude: -71.2608863, latitude: 42.3632783, description: "Leftmost Ziv", is_kind: "dorm", quad_id: Quad.find_by(name: "Ziv").id)
 
 #east buildings
-east = Quad.find_by(name: "East")
-Building.create(name: "Hassenfeld", longitude: -71.2548726, latitude: 42.3679261, description: "Larger building of East that is further uphill", is_kind: "dorm", quad_id: east.id, years: east.years)
-Building.create(name: "Pomerantz", longitude: -71.2547494, latitude: 42.3674802, description: "Smaller building of East that is further downhill", is_kind: "dorm", quad_id: east.id, years: east.years)
+Building.create(name: "Hassenfeld", longitude: -71.2548726, latitude: 42.3679261, description: "Larger building of East that is further uphill", is_kind: "dorm", quad_id: Quad.find_by(name: "East").id)
+Building.create(name: "Pomerantz", longitude: -71.2547494, latitude: 42.3674802, description: "Smaller building of East that is further downhill", is_kind: "dorm", quad_id: Quad.find_by(name: "East").id)
 
-#massell buildings
-massell = Quad.find_by(name: "Massell")
-Building.create(name: "Shapiro", longitude: -71.2614192, latitude: 42.3668253, description: "leftmost in massell quad", is_kind: "dorm", quad_id: massell.id, years: massell.years)
-Building.create(name: "Deroy", longitude: -71.2616125, latitude: 42.3673553, description: "to the right of Shapiro", is_kind: "dorm", quad_id: massell.id, years: massell.years)
-Building.create(name: "Usen", longitude: -71.2605758, latitude: 42.3671662, description: "rightmost in massell quad", is_kind: "dorm", quad_id: massell.id, years: massell.years)
-Building.create(name: "Renfield", longitude: -71.2610296, latitude: 42.3674719, description: "to the left of Usen", is_kind: "dorm", quad_id: massell.id, years: massell.years)
+#massel buildings
+Building.create(name: "Shapiro", longitude: -71.2614192, latitude: 42.3668253, description: "leftmost in massell quad", is_kind: "dorm", quad_id: Quad.find_by(name: "Massell").id)
+Building.create(name: "Deroy", longitude: -71.2616125, latitude: 42.3673553, description: "to the right of Shapiro", is_kind: "dorm", quad_id: Quad.find_by(name: "Massell").id)
+Building.create(name: "Usen", longitude: -71.2605758, latitude: 42.3671662, description: "rightmost in massell quad", is_kind: "dorm", quad_id: Quad.find_by(name: "Massell").id)
+Building.create(name: "Renfield", longitude: -71.2610296, latitude: 42.3674719, description: "to the left of Usen", is_kind: "dorm",quad_id: Quad.find_by(name: "Massell").id)
 
 #ridgewood buildings
-ridgewood = Quad.find_by(name: "Ridgewood")
-Building.create(name: "Ridgewood A", longitude: -71.2604928, latitude: 42.3640341, description: "Ridgewood with the large common area", is_kind: "dorm", quad_id: ridgewood, years: ridgewood.years)
-Building.create(name: "Ridgewood B", longitude: -71.2611426, latitude: 42.3638964, description: "Ridgewood next to Spingold", is_kind: "dorm", quad_id: ridgewood, years: ridgewood.years)
-Building.create(name: "Ridgewood C", longitude: -71.2607127, latitude: 42.3635757, description: "Ridgewood closest to Dunkin Donuts", is_kind: "dorm", quad_id: ridgewood, years: ridgewood.years)
+Building.create(name: "Ridgewood A", longitude: -71.2604928, latitude: 42.3640341, description: "Ridgewood with the large common area", is_kind: "dorm", quad_id: Quad.find_by(name: "Ridgewood").id)
+Building.create(name: "Ridgewood B", longitude: -71.2611426, latitude: 42.3638964, description: "Ridgewood next to Spingold", is_kind: "dorm", quad_id: Quad.find_by(name: "Ridgewood").id)
+Building.create(name: "Ridgewood C", longitude: -71.2607127, latitude: 42.3635757, description: "Ridgewood closest to Dunkin Donuts", is_kind: "dorm", quad_id: Quad.find_by(name: "Ridgewood").id)
 
 #north buildings
-north = Quad.find_by(name: "North")
-Building.create(name: "Gordon", longitude: -71.2559024, latitude: 42.3696982, description: "Connected to the lounge through the first floor.", is_kind: "dorm", quad_id: north.id, years: north.years)
-Building.create(name: "Scheffres", longitude: -71.2563113, latitude: 42.3692478, description: "Connected to the lounge through the basement.", is_kind: "dorm", quad_id: north.id, years: north.years)
-Building.create(name: "Reitman", longitude: -71.2553477, latitude: 42.3696856, description: "Building with washing machines.", is_kind: "dorm", quad_id: north.id, years: north.years)
-Building.create(name: "Cable", longitude: -71.2552624, latitude: 42.3692278, description: "rightmost building in North Quad.", is_kind: "dorm", quad_id: north.id, years: north.years)
+Building.create(name: "Gordon", longitude: -71.2559024, latitude: 42.3696982, description: "Connected to the lounge through the first floor.", is_kind: "dorm", quad_id: Quad.find_by(name: "North").id)
+Building.create(name: "Scheffres", longitude: -71.2563113, latitude: 42.3692478, description: "Connected to the lounge through the basement.", is_kind: "dorm", quad_id: Quad.find_by(name: "North").id)
+Building.create(name: "Reitman", longitude: -71.2553477, latitude: 42.3696856, description: "Building with washing machines.", is_kind: "dorm", quad_id: Quad.find_by(name: "North").id)
+Building.create(name: "Cable", longitude: -71.2552624, latitude: 42.3692278, description: "rightmost building in North Quad.", is_kind: "dorm", quad_id: Quad.find_by(name: "North").id)
 
 #castle
-castle = Quad.find_by(name: "Usen Castle")
-Building.create(name: "Usen Castle", longitude: -71.2556858, latitude: 42.3672562, description: "Brandeis' infamous castle. It can be quite confusing to navigate...", is_kind: "dorm", quad_id: castle.id, years: castle.years)
+Building.create(name: "Usen Castle", longitude: -71.2556858, latitude: 42.3672562, description: "Brandeis' infamous castle. It can be quite confusing to navigate...", is_kind: "dorm", quad_id: Quad.find_by(name: "Usen Castle").id)
 
 #567 South Street
-south_street = Quad.find_by(name: "567 South Street")
-Building.create(name: "567 South Street", longitude: -71.2617369, latitude: 42.3613702, description: "A single appartment style building next to Cappy's Pizza", is_kind: "dorm", quad_id: south_street.id, years: south_street.years)
+Building.create(name: "567 South Street", longitude: -71.2617369, latitude: 42.3613702, description: "A single appartment style building next to Cappy's Pizza", is_kind: "dorm", quad_id: Quad.find_by(name: "567 South Street").id)
 
 #grad buildings
-grad = Quad.find_by(name: "Charles River Apartments")
-Building.create(name: "Morton May, 110 Angleside Road", longitude: -71.2576588, latitude: 42.3595228, description: "One of four grad dorms", is_kind: "dorm", quad_id: grad.id, years: grad.years)
-Building.create(name: "Max and Ann Coffman, 150 Charles River Road", longitude: -71.2572095, latitude: 42.3596814, description: "One of four grad dorms", is_kind: "dorm", quad_id: grad.id, years: grad.years)
-Building.create(name: "Ollie A. Cohen, 164 Charles River Road", longitude: -71.256805, latitude: 42.3599771, description: "One of four grad dorms", is_kind: "dorm", quad_id: grad.id, years: grad.years)
-Building.create(name: "George E. Lewis, 178 Charles River Road", longitude: -71.2566022, latitude: 42.3603332, description: "One of four grad dorms", is_kind: "dorm", quad_id: grad.id, years: grad.years)
+Building.create(name: "Morton May, 110 Angleside Road", longitude: -71.2576588, latitude: 42.3595228, description: "One of four grad dorms", is_kind: "dorm", quad_id: Quad.find_by(name: "Charles River Apartments").id)
+Building.create(name: "Max and Ann Coffman, 150 Charles River Road", longitude: -71.2572095, latitude: 42.3596814, description: "One of four grad dorms", is_kind: "dorm", quad_id: Quad.find_by(name: "Charles River Apartments").id)
+Building.create(name: "Ollie A. Cohen, 164 Charles River Road", longitude: -71.256805, latitude: 42.3599771, description: "One of four grad dorms", is_kind: "dorm", quad_id: Quad.find_by(name: "Charles River Apartments").id)
+Building.create(name: "George E. Lewis, 178 Charles River Road", longitude: -71.2566022, latitude: 42.3603332, description: "One of four grad dorms", is_kind: "dorm", quad_id: Quad.find_by(name: "Charles River Apartments").id)
 
 #mods Buildings
-mods = Quad.find_by(name: "Foster Mods Housing")
-Building.create(name: "Upper Mods", longitude: -71.2538398, latitude: 42.3662074, description: "higher elevation mod area", is_kind: "dorm", quad_id: mods.id, years: mods.years)
-Building.create(name: "Lower Mods", longitude: -71.2534663, latitude: 42.3657189, description: "lower elevation mod area", is_kind: "dorm", quad_id: mods.id, years: mods.years)
+Building.create(name: "Upper Mods", longitude: -71.2538398, latitude: 42.3662074, description: "higher elevation mod area", is_kind: "dorm", quad_id: Quad.find_by(name: "Foster Mods Housing").id)
+Building.create(name: "Lower Mods", longitude: -71.2534663, latitude: 42.3657189, description: "lower elevation mod area", is_kind: "dorm", quad_id: Quad.find_by(name: "Foster Mods Housing").id)
 
 #rosenthal buildings
-rosie = Quad.find_by(name: "Rosenthal")
-Building.create(name: "North", longitude: -71.2601768, latitude: 42.3673301, description: "building at the bottom of the hill", is_kind: "dorm", quad_id: rosie.id, years: rosie.years)
-Building.create(name: "East", longitude: -71.259926, latitude: 42.3670715, description: "building on the side of the hill", is_kind: "dorm", quad_id: rosie.id, years: rosie.years)
-Building.create(name: "South", longitude: -71.2601182, latitude: 42.3668881, description: "building at the top of the hill", is_kind: "dorm", quad_id: rosie.id, years: rosie.years)
+Building.create(name: "North", longitude: -71.2601768, latitude: 42.3673301, description: "building at the bottom of the hill", is_kind: "dorm", quad_id: Quad.find_by(name: "Rosenthal").id)
+Building.create(name: "East", longitude: -71.259926, latitude: 42.3670715, description: "building on the side of the hill", is_kind: "dorm", quad_id: Quad.find_by(name: "Rosenthal").id)
+Building.create(name: "South", longitude: -71.2601182, latitude: 42.3668881, description: "building at the top of the hill", is_kind: "dorm", quad_id: Quad.find_by(name: "Rosenthal").id)
 
 #village buildings
 Building.create(name: "Village A", longitude: -71.2600058, latitude: 42.3636824, description: "Village next to the music building", is_kind: "dorm", quad_id: Quad.find_by(name: "Village").id, years: "0100")
@@ -107,33 +97,27 @@ Building.all.each do |b|
 	picture.save
 end
 
-<<<<<<< HEAD
 # =========== REVIEWS ==============#
-# $redis.flushall
+
 CSV.foreach("db/reviews.csv") do |row|
 		user = User.create( user_name: Faker::Internet.user_name,
 				password_digest: BCrypt::Password.create(Faker::Internet.password),
 				email: Faker::Internet.email,
 				icon_file_name: Faker::Avatar.image
 		)
-		building_id = Building.find_by(name: row[1]).id
     review = Review.create(text: "#{row[5]}\n#{row[7]}",
         rating: (row[4].to_f / 2),
         user_id: user.id,
-        building_id: building_id,
+        building_id: Building.find_by(name: row[1]).id,
         created_at: row[0],
         updated_at: row[0])
     unless row[6].nil?
       tags = row[6].gsub(/\s+/, "").split("#")
       review.tag_list.add(tags)
 			review.save
-			args = {building_id: building_id, tags: tags }
-			# TagCounterJob.perform_async(args)
     end
 end
 
-=======
->>>>>>> 2cedfd4d545a3be5487eed5d26e204eba62dfa34
 # ============= ROOMS ============= #
 building = 0
 
@@ -143,7 +127,7 @@ CSV.foreach("db/room_data.csv") do |row|
     has_ac = true
 
     if row[0].include? "&"
-        building = Building.find_by(name: "#{row[1]}").id
+        building = Building.find_by(name: "#{row[1]}")
     else
         if row[7].include? "false"
             has_kitchen = false
@@ -156,46 +140,9 @@ CSV.foreach("db/room_data.csv") do |row|
         if row[9].include? "false"
             has_ac = false
         end
-        Room.create(number: "#{row[0]}", building_id: building, floor: "#{row[1]}",
-            years_available: "#{row[2]}", room_type: "#{row[3]}", gender: "#{row[4]}",
-            price: "#{row[5]}", rough_housing_num: "#{row[6]}", kitchen: has_kitchen,
-            laundry: has_laundry, ac: has_ac, area: "#{row[11]}", location: "#{row[12]}",
-            cleanliness: 3, noisiness: 3, light: 3, social: 3, issues: 3, general_rating: 3)
+        Room.create(number: "#{row[0]}", building_id: building, floor: "#{row[1]}", years_available: "#{row[2]}", room_type: "#{row[3]}", gender: "#{row[4]}", price: "#{row[5]}", rough_housing_num: "#{row[6]}", kitchen: has_kitchen, laundry: has_laundry, ac: has_ac, area: "#{row[11]}")
     end
 end
-
-# =========== REVIEWS ==============#
-$redis.flushall
-CSV.foreach("db/reviews.csv") do |row|
-		user = User.create( user_name: Faker::Internet.user_name,
-				password_digest: BCrypt::Password.create(Faker::Internet.password),
-				email: Faker::Internet.email,
-				icon_file_name: Faker::Avatar.image
-		)
-		building = Building.find_by(name: row[1])
-		room = Room.find_by(building_id: building.id, number: row[2])
-		if room.nil?
-			room = Room.create(number: row[2], building_id: building.id, years_available: building.years)
-		end
-    review = Review.create(text: "#{row[5]}\n#{row[7]}",
-        rating: (row[4].to_f / 2),
-        user_id: user.id,
-        building_id: building.id,
-				room_id: room.id,
-        created_at: row[0],
-        updated_at: row[0])
-    unless row[6].nil?
-      tags = row[6].gsub(/\s+/, "").split("#")
-      review.tag_list.add(tags)
-			review.save
-			args = {building_id: building.id, tags: tags }
-			TagCounterJob.perform_async(args)
-    end
-
-		args = {room: room, cleanliness: row[8].to_f, noisiness: row[9].to_f, light: row[11].to_f, socialness: row[10].to_f}
-		RoomCounterJob.perform_async(args)
-end
-
 
 # (0..100).each do |i|
 # 	a_user_name = Faker::Internet.user_name
