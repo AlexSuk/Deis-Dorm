63c7ee09bf3a755bb7c90d0e79e44e476f2e98ea
ctakahashi <tachi6@brandeis.edu>
2016-05-04 17:37:49 -0400

    Add more stars everywhere, Connect reviews to rooms, Show Most___ Reviews

--- app/controllers/quads_controller.rb
+++ app/controllers/quads_controller.rb
@@ -10,17 +10,26 @@ class QuadsController < ApplicationController
   end
 
 	def show
-		puts "SHOW", params
 		@quad = Quad.find(params[:id])
+		building_ids = @quad.buildings.map { |b| b.id }
+		all_building_reviews = Review.where(building_id: building_ids)
+		@pos = all_building_reviews.order("rating DESC").first
+		@pos_user = @pos.nil? ? nil : User.find(@pos.user_id)
+		@neg = all_building_reviews.order("rating ASC").first
+		@neg_user = @neg.nil? ? nil : User.find(@neg.user_id)
+		@recent = all_building_reviews.order("updated_at DESC").first
+		@recent_user = @recent.nil? ? nil : User.find(@recent.user_id)
 		@buildings = @quad.buildings
 		@picture = Picture.new
 		@building_tags = {}
+		@building_ratings = {}
 		@buildings.each do |build|
 			tags = $redis.hgetall("b_#{build.id}")
 			top_tags = tags.sort_by { |k,v| -v }.reverse.to_h
 			@building_tags[build.id] = top_tags
+			avg = build.reviews.average(:rating)
+			unless avg.nil? then @building_ratings[build.id] = (avg * 2).round / 2.0 end
 		end
-
 	end
 
 	def add_tags
@@ -31,9 +40,7 @@ class QuadsController < ApplicationController
 			review.save
 			TagCounterJob.perform_async(args)
 		end
-		if params[:format] == "3"
-			render 'add_tags'
-		end
+		if params[:format] == "3" then render 'add_tags' end
 		# respond_to do |format|
 		# 	# format.js { render "alert('Hello Rails');" }
 		# 	format.html { render :partial => 'new_post' }
@@ -44,10 +51,7 @@ class QuadsController < ApplicationController
 		puts "create_post"
 		@quad = params[:quad_id]
 		@building_id = params[:building]
-		# room_num = building.rooms.where("number LIKE ?", "%#{params[:room]}%")
-		# room = building.rooms.where(number: params[:room])
-		# if room.nil?
-
+		room = Room.where("building_id = ? AND number LIKE ?", @building_id, "%#{params[:room]}%").first
 		content = params[:content]
 
 		tags = content.split.find_all{|word| /^#.+/.match word}
@@ -56,11 +60,8 @@ class QuadsController < ApplicationController
 		@review = Review.create(text: content,
 		 						user_id: session[:user_id],
 								building_id: @building_id,
-								room_id: params[:room],
+								room_id: room.nil? ? nil : room.id,
 								rating: params[:rating])
-		# else
-		# 	puts "NO ROOM FOUND!"
-		# end
 		tags = tags.map { |t|
 			t[1..-1]
 	  }
