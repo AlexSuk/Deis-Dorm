9e0335c7ca5d4a6c2ee94acaae248e7ba8225eb8
ctakahashi <tachi6@brandeis.edu>
2016-05-04 21:35:30 -0400

    Change front page from search bar to word cloud

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 class HomeController < ApplicationController
 	skip_before_action :authorize
   def index
-
+		puts "GOEJFISOJFES"
+		gon.cloud = Array.new
+		gon.counter = 0
+		tags = Tag.order("taggings_count DESC").limit(30)
+		tags.each do |tag|
+			if tag.name == "thinwalls"
+				gon.cloud.push({text: tag.name, weight: tag.taggings_count/3})
+			else
+				gon.cloud.push({text: tag.name, weight: tag.taggings_count})
+			end
+		end
   end
 
 end
