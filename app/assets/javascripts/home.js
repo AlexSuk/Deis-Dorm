ec6c4f96874cfb4da218f7cf20cd7dc7a21f90b7
ctakahashi <tachi6@brandeis.edu>
2016-05-06 12:50:26 -0400

    Fix home page size issue

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   if(gon.cloud && gon.counter == 0) {
     gon.counter += 1
     $("#tag-cloud").jQCloud(gon.cloud, {
-      width: 2750,
-      height: 2000,
+      width: 500,
+      height: 330,
       removeOverflowing: true
     });
   }
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,10 +6,7 @@ class Tag < ActiveRecord::Base
     unless text.strip.empty?
       tags = tags.merge Hash[($tgr.get_nouns(tagged)).sort_by { |k,v| -v }[0..1]]
       verb_tags = Hash[($tgr.get_infinitive_verbs(tagged).merge($tgr.get_past_tense_verbs(tagged).merge($tgr.get_gerund_verbs(tagged).merge($tgr.get_passive_verbs(tagged).merge($tgr.get_base_present_verbs(tagged).merge($tgr.get_present_verbs(tagged)){|key, first, second| first + second }) { |key, first, second| first + second }){ |key, first, second| first + second }){ |key, first, second| first + second }){ |key, first, second| first + second })]
-      # verb_tags.delete("'m")
-      # verb_tags.delete("was")
-      # verb_tags.delete("is")
-      tags = tags.merge Hash[verb_tags.except("'m", "was", "is", "are", "do").sort_by { |k,v| -v }[0..0]]
+      tags = tags.merge Hash[verb_tags.except("'m", "was", "is", "are", "do", "'s").sort_by { |k,v| -v }[0..0]]
       tags = tags.merge Hash[($tgr.get_adjectives(tagged)).sort_by { |k,v| -v }[0..2]]
     end
   end
